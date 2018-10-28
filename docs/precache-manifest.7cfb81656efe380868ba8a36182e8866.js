2fc6c6d76ce412a9bc83fff321ba41827dd17d9f
Bruno Sabot <brunosabot@gmail.com>
2018-10-28 14:28:31 +0100

    Update docs

---
+++ docs/precache-manifest.7cfb81656efe380868ba8a36182e8866.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "674d9d35ab7539fdb20307c4867a13b0",
+    "url": "/bdxio-timekeeper/static/media/settings.674d9d35.svg"
+  },
+  {
+    "revision": "5924881cdb4163556797480ce5af5774",
+    "url": "/bdxio-timekeeper/static/media/home.5924881c.svg"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/bdxio-timekeeper/static/media/close.777b93c6.svg"
+  },
+  {
+    "revision": "1f8992f627143e1a4776",
+    "url": "/bdxio-timekeeper/static/js/runtime~main.1f8992f6.js"
+  },
+  {
+    "revision": "deb3ad2c68366ebd0d4b",
+    "url": "/bdxio-timekeeper/static/js/main.deb3ad2c.chunk.js"
+  },
+  {
+    "revision": "e8740195bbf867804876",
+    "url": "/bdxio-timekeeper/static/js/1.e8740195.chunk.js"
+  },
+  {
+    "revision": "deb3ad2c68366ebd0d4b",
+    "url": "/bdxio-timekeeper/static/css/main.f948b6a6.chunk.css"
+  },
+  {
+    "revision": "ce5f22a0fd838be40eaeb7af921fc2d7",
+    "url": "/bdxio-timekeeper/index.html"
+  }
+];
